3f0fdd2280dd1d9eb4ac53725de45b3aad6b9591
dawn-ly <102023742+dawn-ly@users.noreply.github.com>
2023-05-10 20:46:08 +0800

    Update Blockchain.js

--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -6,17 +6,40 @@ class Blockchain {
       - 创世区块
       - 存储区块的映射
   */
-  constructor() {}
-
+ constructor(name) {
+  this.name = name;
+  this.genesis = null;
+  this.blocks = {};
+  }
   // 2. 定义 longestChain 函数
   /* 
     返回当前链中最长的区块信息列表
   */
-  longestChain() {
-
-
-    return []
+    longestChain() {
+      if (this.genesis == null) {
+        return [];
+      }
+      let blockKeys = Object.keys(this.blocks);
+      let maxLen = blockKeys.length;
+      let longestChain = [];
+      for (let i = 0; i < maxLen; i++) {
+        let chain = [this.blocks[blockKeys[i]]];
+        let previousBlock = this.blocks[blockKeys[i]];
+        while (previousBlock && previousBlock.previousHash !== "0") {
+          let block = Object.values(this.blocks).filter(
+            (block) => block.hash === previousBlock.previousHash
+          )[0];
+          if (!block) {
+            break;
+          }
+          chain.push(block);
+          previousBlock = block;
+        }
+        if (chain.length > longestChain.length) {
+          longestChain = chain;
+        }
+      }
+      return longestChain;
+    }
   }
-}
-
 export default Blockchain
