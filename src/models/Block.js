e73da0332e5a15c029abc5c930fe9b0e72bb5b32
dawn-ly <102023742+dawn-ly@users.noreply.github.com>
2023-05-19 22:25:09 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -2,15 +2,41 @@ import sha256 from 'crypto-js/sha256.js'
 
 export const DIFFICULTY = 2
 
-class Block {
-  // 1. 完成构造函数及其参数
 
-  constructor() {}
+  class Block {
+  constructor(data, previousHash) {
+    this.timestamp = Date.now();
+    this.data = data;
+    this.previousHash = previousHash;
+    this.nonce = 0;
+    this.hash = this.calculateHash();
+    this.difficulty = 4; // 挖矿难度
+  }
 
-  isValid() {}
+  // 计算区块的哈希值
+  calculateHash() {
+    const crypto = require('crypto');
+    const hash = crypto.createHash('sha256');
+    hash.update(this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce);
+    return hash.digest('hex');
+  }
 
-  setNonce(nonce) {}
-  
+  // 验证区块是否符合挖矿难度要求
+  isValid() {
+    const prefix = '0'.repeat(this.difficulty);
+    return this.hash.startsWith(prefix);
+  }
+
+  // 更新 Nonce 值
+  setNonce(nonce) {
+    this.nonce = nonce;
+    this.hash = this.calculateHash();
+  }
+
+  // 更新 Hash 值
+  updateHash() {
+    this.hash = this.calculateHash();
+
 }
 
 export default Block
