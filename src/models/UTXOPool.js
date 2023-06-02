7567f50677bb7f245ed8329ba6c154a74f736b48
dawn-ly <102023742+dawn-ly@users.noreply.github.com>
2023-06-03 00:06:46 +0800

    Update UTXOPool.js

--- src/models/UTXOPool.js
+++ src/models/UTXOPool.js
@@ -1,21 +1,68 @@
 import UTXO from './UTXO.js'
+import sha256 from 'crypto-js/sha256.js'
 
 class UTXOPool {
-  constructor(utxos = {}) {}
+  constructor(utxos = {}) {
+    this.utxos = { ...utxos }
+  }
 
-  addUTXO(publicKey, amount) {}
+  addUTXO(txHash, outputIndex, output) {
+    const utxo = new UTXO(txHash, outputIndex, output)
+    this.utxos[utxo.toString()] = utxo
+  }
 
-  clone() {}
+  removeUTXO(txHash, outputIndex) {
+    const utxo = new UTXO(txHash, outputIndex)
+    delete this.utxos[utxo.toString()]
+  }
 
-  // 处理交易函数
-  handleTransaction() {}
+  getUTXO(txHash, outputIndex) {
+    const utxo = new UTXO(txHash, outputIndex)
+    return this.utxos[utxo.toString()]
+  }
 
-  // 验证交易合法性
-  /**
-   * 验证余额
-   * 返回 bool 
-   */
-  isValidTransaction() {}
+  getAllUTXOs() {
+    return Object.values(this.utxos)
+  }
+
+  clone() {
+    return new UTXOPool(this.utxos)
+  }
+
+  isValidTransaction(senderAddress, amount) {
+    // 在此处实现您的逻辑来验证交易是否有效
+    // 根据您的业务需求编写代码，例如检查发送方地址的余额是否足够等
+    // 返回 true 或 false，表示交易的有效性
+
+    // 示例逻辑：检查发送方地址在 UTXO 池中是否存在，并且余额是否足够
+    const senderUTXO = this.utxos[senderAddress];
+    if (!senderUTXO || senderUTXO.amount < amount) {
+      return false;
+    }
+
+    return true;
+  }
+  handleTransaction(transaction) {
+    const senderAddress = transaction.senderAddress
+    const amount = transaction.amount
+
+    if (!this.isValidTransaction(senderAddress, amount)) {
+      return false
+    }
+
+    // 示例逻辑：更新 UTXO 池
+    const senderUTXO = this.utxos[senderAddress]
+    senderUTXO.output.amount -= amount
+
+    // 处理新的交易输出
+    const newUTXO = new UTXO(transaction.txHash, transaction.outputIndex, {
+      address: transaction.recipientAddress,
+      amount: amount
+    })
+    this.addUTXO(newUTXO.txHash, newUTXO.outputIndex, newUTXO.output)
+
+    return true
+  }
 }
 
-export default UTXOPool
+export default UTXOPool;
