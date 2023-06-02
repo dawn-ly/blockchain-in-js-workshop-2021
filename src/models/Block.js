fb6020c76e066da1115a294a4238e88c955fc6d3
dawn-ly <102023742+dawn-ly@users.noreply.github.com>
2023-06-03 00:04:10 +0800

    Update Block.js

--- src/models/Block.js
+++ src/models/Block.js
@@ -3,13 +3,33 @@ import sha256 from 'crypto-js/sha256.js'
 export const DIFFICULTY = 2
 
 class Block {
-  // 1. 完成构造函数及其参数
+  constructor(blockchain, prevHash, height, data, coinbaseBeneficiary, transactions = []) {
+    this.blockchain = blockchain
+    this.prevHash = prevHash
+    this.height = height
+    this.data = data
+    this.coinbaseBeneficiary = coinbaseBeneficiary
+    this.transactions = transactions
+    this.timestamp = new Date().getTime()
+    this.nonce = 0
+    this.hash = this.calculateHash()
+  }
 
-  constructor() {}
+  calculateHash() {
+    return sha256(
+      this.prevHash + this.height.toString() + this.data + this.nonce.toString()
+    ).toString()
+  }
 
-  isValid() {}
+  isValid() {
+    const leadingZeros = '0'.repeat(DIFFICULTY)
+    return this.hash.substring(0, DIFFICULTY) === leadingZeros
+  }
 
-  setNonce(nonce) {}
+  setNonce(nonce) {
+    this.nonce = nonce
+    this.hash = this.calculateHash()
+  }
 }
 
 export default Block
