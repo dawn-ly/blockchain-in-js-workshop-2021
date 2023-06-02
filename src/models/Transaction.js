94dd4fb5b32b8cc7b43b08873a288da19503bc2d
dawn-ly <102023742+dawn-ly@users.noreply.github.com>
2023-06-03 00:06:25 +0800

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,19 +1,24 @@
 import sha256 from 'crypto-js/sha256.js'
 
-
 class Transaction {
-  constructor() {
-    
-  }
-
-  // 更新交易 hash
-  _setHash() {
-
+  constructor(senderAddress, recipientAddress, amount) {
+    this.senderAddress = senderAddress
+    this.recipientAddress = recipientAddress
+    this.amount = amount
+    this.timestamp = new Date().getTime()
+    this.txHash = this._calculateHash()
   }
 
   // 计算交易 hash 的摘要函数
   _calculateHash() {
-   
+    return sha256(
+      this.senderAddress + this.recipientAddress + this.amount.toString() + this.timestamp.toString()
+    ).toString()
+  }
+
+  // 更新交易 hash
+  _setHash() {
+    this.txHash = this._calculateHash()
   }
 }
 
