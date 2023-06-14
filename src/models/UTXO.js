6d61c6e19035d8158b6347954fbbdf20c98969e3
dawn-ly <102023742+dawn-ly@users.noreply.github.com>
2023-06-14 22:16:54 +0800

    Update UTXO.js

--- src/models/UTXO.js
+++ src/models/UTXO.js
@@ -1,3 +1,9 @@
-export default class UTXO {
-  constructor() {}
+
+class UTXO {
+  constructor(address, amount) {
+    this.address = address
+    this.amount = amount
+  }
 }
+
+export default UTXO
