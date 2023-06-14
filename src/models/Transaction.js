8a68e246d641033e2606c5d935323e7567ae7e15
dawn-ly <102023742+dawn-ly@users.noreply.github.com>
2023-06-14 22:16:40 +0800

    Update Transaction.js

--- src/models/Transaction.js
+++ src/models/Transaction.js
@@ -1,25 +1,42 @@
 import sha256 from 'crypto-js/sha256.js'
+import {verifySignature} from '../crypto.js'
+
 
 class Transaction {
-  constructor() {
-
-  }
-
-  // 更新交易 hash
-  _setHash() {
-
+  constructor(senderAddress, recipientAddress, amount, fee, signature) {
+    this.senderAddress = senderAddress
+    this.recipientAddress = recipientAddress
+    this.amount = amount
+    this.fee = fee
+    this.timestamp = new Date().getTime()
+    this.signature = signature
+    this.txHash = this._calculateHash()
   }
 
   // 计算交易 hash 的摘要函数
   _calculateHash() {
-
+    return sha256(
+      this.senderAddress +
+      this.recipientAddress +
+      this.amount.toString() +
+      this.fee.toString() +
+      this.timestamp.toString()
+    ).toString()
   }
 
-  // 校验交易签名 返回 bool 类型的值
   hasValidSignature() {
-
+    const message = this.senderAddress +
+      this.recipientAddress +
+      this.amount.toString() +
+      this.fee.toString() +
+      this.timestamp.toString()
+      
+    return verifySignature(message, this.signature, this.senderAddress)
   }
-
+  // 更新交易 hash
+  _setHash() {
+    this.txHash = this._calculateHash()
+  }
 }
 
 export default Transaction
